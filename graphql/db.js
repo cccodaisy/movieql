a082684a021bc32d091d2d1ad5754f59690eae8d
cccodaisy <58411126+cccodaisy@users.noreply.github.com>
2020-03-29 18:28:40 +0900

    #2.0 Make a Movie API

--- graphql/db.js
+++ graphql/db.js
@@ -1,33 +1,49 @@
-export const people = [
+let movies = [
     {
-        id: "0",
-        name: "Nicolas",
-        age: 18,
-        gender: "female"
-    },{
-        id: "1",
-        name: "Risa",
-        age: 18,
-        gender: "male"
-    },{
-        id: "2",
-        name: "Elsa",
-        age: 29,
-        gender: "female"
-    },{
-        id: "3",
-        name: "Jay",
-        age: 17,
-        gender: "male"
-    },{
-        id: "4",
-        name: "Su",
-        age: 48,
-        gender: "female"
+      id: 0,
+      name: "Star Wars - The new one",
+      score: 1
+    },
+    {
+      id: 1,
+      name: "Avengers - The new one",
+      score: 8
+    },
+    {
+      id: 2,
+      name: "The Godfather I",
+      score: 99
+    },
+    {
+      id: 3,
+      name: "Logan",
+      score: 2
     }
-];
-
-export const getById = id => {
-    const filteredPeople = people.filter(person => person.id === String(id));
-    return filteredPeople[0];
-} 
+  ];
+  
+  export const getMovies = () => movies;
+  
+  export const getById = id => {
+    const filteredMovies = movies.filter(movie => movie.id === id);
+    return filteredMovies[0];
+  };
+  
+  export const deleteMovie = id => {
+    const cleanedMovies = movies.filter(movie => movie.id !== id);
+    if (movies.length > cleanedMovies.length) {
+      movies = cleanedMovies;
+      return true;
+    } else {
+      return false;
+    }
+  };
+  
+  export const addMovie = (name, score) => {
+    const newMovie = {
+      id: parseInt(`${movies.length + 1}`),
+      name,
+      score
+    };
+    movies.push(newMovie);
+    return newMovie;
+  };
